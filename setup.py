aa82948faa1475d930daae59e964e60a21cc7dd9
alwaysvivek <vivekdagar@zohomail.in>
2026-03-01 17:34:07 +0530

    Bump version to 2.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ class CustomInstall(install):
 
 setup(
     name="xbasic-8bit",
-    version="2.0.0",
+    version="2.0.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
